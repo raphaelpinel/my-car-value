951b2e0f57b6dd27b534c7eac5bab24d17c5ae0f
Raphael Pinel <36964192+raphaelpinel@users.noreply.github.com>
2022-12-11 14:15:16 +0200

    Add repository in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  repo: Repository<User>;
+
+  constructor(repo: Repository<User>) {
+    this.repo = repo;
+  }
+}
