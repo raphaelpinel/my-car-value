c48664f9a9731f7faa1cdc8c1a8ecfddc1643d4c
Raphael Pinel <36964192+raphaelpinel@users.noreply.github.com>
2022-12-11 15:37:36 +0200

    Create user to database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log('body:', body);
+    const { email, password } = body;
+    this.usersService.create(email, password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  repo: Repository<User>;
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  constructor(repo: Repository<User>) {
-    this.repo = repo;
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
   }
 }
